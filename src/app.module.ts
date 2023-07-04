4a0ac247c3e5f4d1ff017708e4f7d22d662f5dd4
Xielain <rklimow409@gmail.com>
2023-07-04 18:07:45 +0300

    generated prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SkillModule } from './skill/skill.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), SkillModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
